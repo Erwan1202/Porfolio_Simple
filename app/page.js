a4ee2af1e030f70554bb70554c21b291893a61c4
erwan <erwan.a.marechal@gmail.com>
2025-08-14 12:09:32 +0200

    feat: add About section to the Page component

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 import Hero from './sections/Hero';
-
+import About from './sections/About';
 export default function Page() {
     return (
         <>
             <Hero />
-            
+            <About />
         </>
     );
 }
