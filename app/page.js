d78d318b407a38ad5acaea0cf772f1d37cd286bf
erwan <erwan.a.marechal@gmail.com>
2025-08-14 11:59:53 +0200

    feat: add Hero section with introductory content to the page

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Hero from './sections/Hero';
+import About from './sections/About';
+import Projects from './sections/Projects';
+import Contact from './sections/Contac';
+
+export default function Page() {
+    return (
+        <>
+            <Hero />
+            <About />
+            <Projects />
+            <Contact />
+        </>
+    );
+}
