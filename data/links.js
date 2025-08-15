7f60fddcc671c4108e67334a0c849a77847eb0e2
erwan <erwan.a.marechal@gmail.com>
2025-08-15 09:46:31 +0200

    feat: update email address in links.js

--- data/links.js
+++ data/links.js
@@ -1,7 +1,5 @@
 // src/data/links.js
 export const links = {
-  email: "erwan.marechal@exemple.com", // ⇐ remplace par ton email
+  email: "erwan.a.marechal@gmail.com",
   github: "https://github.com/Erwan1202",
-  linkedin: "https://www.linkedin.com/in/erwan-marechal/", // ⇐ remplace
-  x: "https://x.com/" // ⇐ optionnel
 };
